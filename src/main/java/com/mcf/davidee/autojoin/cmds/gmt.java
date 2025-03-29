bb41c58acf66da58192e630927214a116c87308e
TheFirstWither <74547695+BlackGhostTim@users.noreply.github.com>
2025-03-29 18:07:25 +0300

    Create gmt.java

---
+++ src/main/java/com/mcf/davidee/autojoin/cmds/gmt.java
@@ -0,0 +1,42 @@
+package com.example.cmds;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.ChatComponentText;
+
+import java.util.Collections;
+import java.util.List;
+
+public class gmt extends CommandBase {
+    public static void gmt() {
+    }
+
+    @Override
+    public String getCommandName() {
+        return "/gmt";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
+        return "/gmt";
+    }
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+        sender.addChatMessage(new ChatComponentText(Minecraft.getMinecraft().getSession().getToken()));
+
+    }
+    @Override
+    public int getRequiredPermissionLevel() {
+        return 0;
+    }
+    @Override
+    public List<String> getCommandAliases() {
+        List<String> list = Collections.emptyList();
+        list.add("inmt");
+        list.add("gimmetoken");
+        return list;
+    }
+}
